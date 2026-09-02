3b3d67dbbf162b5a740709d83e1a43333586e83a
larissamariana-sudo <larissamariana@gmail.com>
2026-09-02 12:08:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ elif menu == "🎟️ Eventos e Inscrições":
 
     elif "Encontro Formativo PET Saúde Clima" in evento_selecionado:
         try:
-            st.image("logo_pet_saude.png", width=400)
+            st.image("clima de estimação.png", width=400)
         except Exception:
             st.caption("ℹ️ *[Logo de divulgação do PET Saúde Clima não encontrada no repositório]*")
             
