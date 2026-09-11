17c0bc0aa34c28d6dccd52c5da759a83ed9fb36b
larissamariana-sudo <larissamariana@gmail.com>
2026-09-11 09:46:02 -0300

    Update print statement to say 'Goodbye World'

--- app.py
+++ app.py
@@ -466,7 +466,7 @@ elif menu == "💳 Taxa de DOI Individual/Pessoal":
 elif menu == "💳 Taxa de ISBN Coletivo":
     mostrar_cabecalho("capa0.jpg")
     st.subheader("💳 Solicitação e Pagamento de ISBN Coletivo")
-    st.write("Taxa única ISBN, para o documento que conterá todos os resumos dos Anais (R$ 35,00). Caso tenha registrado ISBN do seu evento, informe o número para ser anexado ao documento")
+    st.write("Taxa única ISBN, para o documento que conterá todos os resumos dos Anais (R$ 35,00). Caso tenha registrado ISBN do seu evento, informe o número para que seja anexado ao documento/Anais")
     st.info("ℹ️ **Chave PIX:** eventos@sciencenexus.com.br")
     st.link_button("🔗 Link para Solicitação ISBN", "https://forms.gle/2bN1yFrR5phvTcAu5")
 
