65bca92b3f763709887cedc13602a027ecce8a01
larissamariana-sudo <larissamariana@gmail.com>
2026-09-12 09:49:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -133,14 +133,10 @@ elif menu == "🎟️ Eventos e Inscrições":
         st.markdown("#### 📅 Programação do Evento")
         st.link_button("📅 Ver / Baixar Programação da Jornada EM BREVE", "COLE_LINK_PROGRAMACAO_JORNADA")
         
-        opcoes_inscricao = ["Participante/Ouvinte", "Orientador", "Apresentador de Trabalho", "Membro da Banca", "Cadastro Resumo Simples (Orientador)", "Cadastro Resumo Expandido (Orientador)"]
+        opcoes_inscricao = ["Participante/Ouvinte", "Cadastro Trabalho para Apresentação (Orientador)"]
         
         link_ouv = "https://forms.gle/tVKQtkEpQHG9Bo3K7"
-        link_ori = "https://forms.gle/tVKQtkEpQHG9Bo3K7"
-        link_apr = "https://forms.gle/tVKQtkEpQHG9Bo3K7"
-        link_ban = "https://forms.gle/tVKQtkEpQHG9Bo3K7"
-        link_sim = "https://form.jotform.com/262543743960664"
-        link_cad = "https://form.jotform.com/262542975983675"
+        link_cad = "https://forms.gle/CP2dG8qZXP2xoRtG6"
 
     elif "Encontro Formativo PET Saúde Clima" in evento_selecionado:
         try:
