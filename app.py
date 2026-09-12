aac84418ad29bfa3c1bb02aa0526ba33f369236d
larissamariana-sudo <larissamariana@gmail.com>
2026-09-12 09:54:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -353,8 +353,10 @@ elif menu == "✍️ Trabalhos Científicos":
 
         st.markdown("---")
         st.info("📌 **Importante:** Para que os arquivos sejam salvos diretamente na nuvem da comissão científica, a submissão é feita por formulário dedicado.")
-        st.link_button("📥 Jornada Científica / Clique aqui para acessar o Formulário de Submissão de Trabalhos", "https://forms.gle/UUmLAAEdCwY9JRrY6")
-        st.link_button("📥 Atividades de Extensão / Clique aqui para acessar o Formulário de Submissão de Trabalhos", "https://forms.gle/7JXdM3jRKzqJyqdR9")
+        st.link_button("📥 Jornada Científica Resumo Expandido/ Clique aqui para acessar o Formulário de Submissão de Trabalhos", "https://form.jotform.com/262542975983675")
+        st.link_button("📥 Jornada Científica Resumo Simples / Clique aqui para acessar o Formulário de Submissão de Trabalhos", "https://form.jotform.com/262543743960664")
+        
+        st.link_button("📥 Atividades de Extensão Relato de Experência • Resumo Expandido/ Clique aqui para acessar o Formulário de Submissão de Trabalhos", "https://forms.gle/7JXdM3jRKzqJyqdR9")
     
     with tab_principal2:
         st.write("Digite o seu e-mail cadastrado na submissão para verificar o parecer atual da comissão científica.")
